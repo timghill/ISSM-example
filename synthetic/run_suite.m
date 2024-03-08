8b068d9fbc91859516acd8848c1b50618da60b0e
Tim <tha111@sfu.ca>
2024-03-08 08:57:11 -0800

    Add header to describe parameter order

--- synthetic/run_suite.m
+++ synthetic/run_suite.m
@@ -1,3 +1,5 @@
+% Format
+% run_job(k_c, k_s, alpha_s, beta_s, omega, jobid)
 run_job(0.5, 0.01, 5/4, 3/2, 0,      1);
 run_job(0.5, 0.05, 3/2, 3/2, 0,      2);
 run_job(0.5, 0.01, 3,   2,   0,      3);
